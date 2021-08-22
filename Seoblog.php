bab40076ecf6580f41850273cd90866e75d65637
fatemekamalvand <fatemeh.ilam1371@gmail.com>
2021-08-22 15:09:42 +0430

    last update 1400/5/31 at 03:09 PM
    last update 1400/5/31 at 03:09 PM

---
+++ Seoblog.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace persianyii\seoblog;
+
+/**
+ * This is just an example.
+ */
+class Seoblog extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
